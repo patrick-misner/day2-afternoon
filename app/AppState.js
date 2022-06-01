0f2c1e92743fc99bda1743b8f7dafb9f9f598cc4
Patrick Misner <misner.patrick@gmail.com>
2022-06-01 15:40:25 -0600

    draw

--- app/AppState.js
+++ app/AppState.js
@@ -4,6 +4,12 @@ import { isValidProp } from "./Utils/isValidProp.js"
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
   values = []
+
+
+    /** @type {import('./Models/WilePokemon').WildPokemon[]} */
+  wildpokemons = []
+
+
 }
 
 export const ProxyState = new Proxy(new AppState(), {
